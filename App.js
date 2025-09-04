688b01fe8af5e8402fc98bb80e853d936de600eb
LuizGustavo007 <luizinhogucpv@gmail.com>
2025-09-04 16:14:45 -0300

    commitluiz

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 // App.js
+import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+
 import SplashScreen from './src/screens/SplashScreen'
 import LoginScreen from './src/screens/LoginScreen'
 import SignupScreen from './src/screens/SignupScreen'
-import HomeScreen from './src/screens/HomeScreen'
+import HomeContent from './src/screens/HomeScreen' // conteúdo da Home
+import PostScreen from './src/screens/PostScreen'
 
 const Stack = createNativeStackNavigator()
+const Tab = createBottomTabNavigator()
+
+// HomeScreen agora é o container das abas
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeContent} />
+      <Tab.Screen name="Post" component={PostScreen} />
+    </Tab.Navigator>
+  )
+}
 
 export default function App() {
   return (
@@ -15,7 +31,7 @@ export default function App() {
         <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeTabs} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   )
